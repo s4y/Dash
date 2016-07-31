ac483df2c36b09d022c92b9a7ec8d91c3d953d8e
Sidney San Martín <sidney@s4y.us>
2016-07-31 19:10:54 -0400

    Initial commit on an old project

---
+++ Dash/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Dash
+//
+//  Created by Sidney San Martín on 12/10/13.
+//  Copyright (c) 2013 s4y. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
